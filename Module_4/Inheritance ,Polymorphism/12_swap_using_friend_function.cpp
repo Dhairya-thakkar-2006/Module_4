1117d86cb85620380fef4f89702fee3dd26e96d3
Dhairya-thakkar-2006 <thakkardhairya47@gmail.com>
2024-10-18 14:16:31 +0530

    Add files via upload

---
+++ Module_4/Inheritance ,Polymorphism/12_swap_using_friend_function.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Swap
+{
+	int S1, S2;
+
+	public :
+			void get()
+			{
+				cout<<"\n\n\t Enter first number : ";
+				cin>>S1;
+				cout<<"\n\n\t Enter second number : ";
+				cin>>S2;
+			}
+
+			friend void swapping(Swap);
+};
+
+void swapping(Swap S)
+{
+	cout<<"\n\n\t Before Swapping : ";
+	cout<<"\n\n\t First Number = "<<S.S1;
+	cout<<"\n\n\t Second Number = "<<S.S2;
+
+	cout<<"\n\n\t After Swapping : ";
+
+	S.S1 = S.S1 + S.S2;
+	S.S2 = S.S1 - S.S2;
+	S.S1 = S.S1 -	 S.S2;
+	cout<<"\n\n\t First Number = "<<S.S1;
+	cout<<"\n\n\t Second Number = "<<S.S2;
+}
+
+main()
+{
+	Swap S;
+
+	S.get();
+	swapping(S);
+}
+
